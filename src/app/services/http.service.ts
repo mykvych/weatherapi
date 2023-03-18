15e97562888dc194087515535c62ecb30908d8d9
Mykhailo Popovych <emubis2001@gmail.com>
2023-03-18 16:39:05 +0200

    changed http to https

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -10,13 +10,13 @@ export class HttpService{
     
   getWeather(coordinates: any){
 
-    this.url = `http://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=c9dd7eeb1316e4aaa7af48ab20b74d22`;
+    this.url = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=c9dd7eeb1316e4aaa7af48ab20b74d22`;
 
     return this.http.get(this.url);
   }
 
   getCountriesAndStates(city: string): Observable<Geolocation[]>{
-    this.url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=c9dd7eeb1316e4aaa7af48ab20b74d22`;
+    this.url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=c9dd7eeb1316e4aaa7af48ab20b74d22`;
 
     return this.http.get<Geolocation[]>(this.url);
   }
